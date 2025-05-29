eafa72a4c1950ac47dba8ffc2eabe6e1bc7b20a4
selt2002 <selt2002@gmail.com>
2025-05-29 19:43:05 -0300

    sin freertos, falta corregir error con una funcion del rtos que eliminamos

--- STA_ejemplo/main/station_example_main.c
+++ STA_ejemplo/main/station_example_main.c
@@ -24,13 +24,14 @@
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_ESP_WIFI_SSID CONFIG_ESP_WIFI_SSID
-#define EXAMPLE_ESP_WIFI_PASS CONFIG_ESP_WIFI_PASSWORD
+
+// #define EXAMPLE_ESP_WIFI_SSID CONFIG_ESP_WIFI_SSID
+// #define EXAMPLE_ESP_WIFI_PASS CONFIG_ESP_WIFI_PASSWORD
 #define EXAMPLE_ESP_MAXIMUM_RETRY CONFIG_ESP_MAXIMUM_RETRY
 
 #if CONFIG_ESP_WPA3_SAE_PWE_HUNT_AND_PECK
 #define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HUNT_AND_PECK
-#define EXAMPLE_H2E_IDENTIFIER ""
+#define EXAMPLE_H2E_IDENTIFIER "wifi_loco"
 #elif CONFIG_ESP_WPA3_SAE_PWE_HASH_TO_ELEMENT
 #define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HASH_TO_ELEMENT
 #define EXAMPLE_H2E_IDENTIFIER CONFIG_ESP_WIFI_PW_ID
@@ -129,8 +130,8 @@ void wifi_init_sta(void)
 
     wifi_config_t wifi_config = {
         .sta = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
-            .password = EXAMPLE_ESP_WIFI_PASS,
+            .ssid = "caliope",      // EXAMPLE_ESP_WIFI_SSID,
+            .password = "sinlugar", // EXAMPLE_ESP_WIFI_PASS,
             /* Authmode threshold resets to WPA2 as default if password matches WPA2 standards (password len => 8).
              * If you want to connect the device to deprecated WEP/WPA networks, Please set the threshold value
              * to WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK and set the password with length and format matching to
@@ -160,12 +161,18 @@ void wifi_init_sta(void)
     if (bits & WIFI_CONNECTED_BIT)
     {
         ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
-                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+                 "caliope" // EXAMPLE_ESP_WIFI_SSID
+                 ,
+                 "sinlugar" // EXAMPLE_ESP_WIFI_PASS
+        );
     }
     else if (bits & WIFI_FAIL_BIT)
     {
         ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+                 "caliope" // EXAMPLE_ESP_WIFI_SSID
+                 ,
+                 "sinlugar" // EXAMPLE_ESP_WIFI_PASS
+        );
     }
     else
     {
